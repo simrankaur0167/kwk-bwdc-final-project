30cc6b10284235347f4e795954bc35c66ffcb89b
simrankaur0167 <kharsimranm@gmail.com>
2025-07-21 19:33:33 -0400

    Added infant mortality data

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import LeftAlignedExample from "./sections/LeftAlignedExample.svelte";
   import RightAlignedExample from "./sections/RightAlignedExample.svelte";
-  import ImageExample from "./sections/MapComponent1.svelte";
   import TitleSection from "./sections/TitleSection.svelte";
   import InteresectionObserverBasicExample from "./sections/InteresectionObserverBasicExample.svelte";
   import IntersectionObserverAlertExample from "./sections/IntersectionObserverAlertExample.svelte";
@@ -10,16 +9,20 @@
   import IntersectionObserverListExample from "./sections/IntersectionObserverListExample.svelte";
   import TextSection from "./sections/TextSection.svelte";
   import EducationalAttainment from "./sections/EducationalAttainment.svelte";
-  import MapComponent from "./sections/MapComponent.svelte";
+  import MapComponent1 from "./sections/MapComponent1.svelte";
+  import MapComponent2 from "./sections/MapComponent2.svelte";
+  import InfantMortality from "./sections/InfantMortality.svelte";
 </script>
 
 <main>
   <TitleSection />
   <TextSection />
   <EducationalAttainment />
+  <MapComponent1 />
+  <MapComponent2 />
+  <InfantMortality />
   <RightAlignedExample />
   <LeftAlignedExample />
-  <ImageExample />
 
   <IntersectionObserverSectionCard />
   <InteresectionObserverBasicExample />
---
+++ src/sections/InfantMortality.svelte
@@ -0,0 +1,74 @@
+<script>
+    import * as Highcharts from "highcharts";
+    import "highcharts/modules/exporting";
+    import { Chart } from "@highcharts/svelte";
+    import Scroller from "../lib/Scroller.svelte";
+    import ArticleText from "../lib/ArticleText.svelte";
+
+    let options = {
+        legend: {
+    enabled: false,
+    },
+    chart: {
+        type: "bar",
+        style: {
+            fontFamily: "Georgia, serif",
+        },
+    },
+    title: {
+        text: "Infant mortality rate",
+    },
+    plotOptions: {
+        column: {
+            dataLabels: {
+                enabled: true,
+                format: "{y}",
+                style: {
+                    fontSize: "1em",
+                    textOutline: "none",
+                },
+            },
+        },
+    },
+    xAxis: {
+        type: "category",
+    },
+    yAxis: {
+        title: {
+            text: "Infant mortality rate",
+        },
+    },
+    series: [
+        {
+            color: "#34495e",
+            name: "Infant mortality rate",
+            data: [
+                { name: "Manhattan", y: 2.8 },
+                { name: "Tompkins", y: 1.4 },
+                { name: "Westchester", y: 2.8 },
+                { name: "Chenango", y: 6.3 },
+                { name: "Wyoming", y: 8.4 },
+                { name: "Orleans.", y: 0.0 },
+            ],
+        },
+    ],
+};
+
+</script>
+
+<div>
+    <Scroller layout="right">
+        {#snippet sticky()}
+            <div class="chart">
+                <Chart {options} highcharts={Highcharts} />
+            </div>
+        {/snippet}
+        {#snippet scrolly()}
+             <ArticleText>
+                A child born in Manhattan, Tompkins, or Westchester has a higher chance of survival. Infant mortality rates are lower in highly educated counties.
+                <br /><br />
+                Source: <a href="https://www.health.ny.gov/statistics/vital_statistics/2021/table45.htm">New York State Department of Health</a>
+            </ArticleText>
+        {/snippet}
+    </Scroller>
+</div>
